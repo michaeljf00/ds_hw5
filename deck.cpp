00467fd1961a3e4e1f8f96c3433a423f32770f96
MikeJoshF <michaeljfababeir@gmail.com>
2020-02-22 23:23:20 -0500

    Added deck.cpp, deck.h, and main.cpp to repository

---
+++ deck.cpp
@@ -0,0 +1,51 @@
+// ==================================================================================
+// Implement all functions that you listed by prototype in deck.h
+// You may include additional helper functions as necessary
+// All of your code must be in deck.h and deck.cpp
+//
+// NOTE: These are standalone functions, not part of a class.
+// ==================================================================================
+
+#include <iostream>
+#include <cassert>
+#include "playing_card.h"
+#include "node.h"
+#include "deck.h"
+
+// ==================================================================================
+
+int DeckSize(Node* deck){
+    int count = 0;
+    Node* curr = deck;
+    while (curr != NULL){
+        curr = curr->after;
+        count += 1;
+    }
+    return count;
+}
+
+
+void PrintDeckSorted(const std::string &description, Node* deck){
+    std::cout << description;
+    Node *tmp = deck;
+    while (tmp != NULL) {
+        std::cout << " " << tmp->getCard().getString();
+        tmp = tmp->sorted_next;
+  }
+  std::cout << std::endl;
+}
+
+bool SamePrimaryOrder(Node* deck1, Node* deck2){
+    if (DeckSize(deck1) != DeckSize(deck2)) return false;
+    Node* currD1 = deck1;
+    Node* currD2 = deck2;
+    while (currD1 != NULL and currD2 != NULL){
+        if (currD1->getCard().getString() != currD2->getCard().getString() ||\
+            currD1->getCard().getCard() != currD2->getCard().getCard()){
+            return false;
+        }
+        currD1 = currD1->after;
+        currD2 = currD2->after;
+    }
+    return true;
+}
---
+++ deck.h
@@ -0,0 +1,28 @@
+#include "playing_card.h"
+#include "node.h"
+
+// ==================================================================================
+// prototypes for provided functions (implemented in main.cpp)
+// ==================================================================================
+
+bool SanityCheckPrimary(Node* top);
+bool SanityCheckSorted(Node* first);
+Node* CreateDeck(int numCards);
+void DeckPushBackCard(Node* &deck, const std::string& suit, int num);
+void PrintDeckPrimary(const std::string &description, Node* deck);
+void PrintDeckSorted(const std::string &description, Node* deck);
+int DeckSize(Node* deck);
+bool SamePrimaryOrder(Node* deck1, Node* deck2);
+
+// ==================================================================================
+//
+// Write all the prototypes for the your functions here
+// (they will be implemented in deck.cpp)
+//
+// NOTE: These are standalone functions, not part of a class.
+//
+// You may include additional helper functions as necessary
+// All of your code must be in deck.h and deck.cpp
+//
+// ==================================================================================
+
